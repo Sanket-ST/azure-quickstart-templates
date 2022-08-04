6ed2193e0d6555dc38abd05fefd76d97fa418962
Sanket-ST <98595691+Sanket-ST@users.noreply.github.com>
2022-08-04 12:38:43 +0530

    Update jbosseap-setup-redhat.sh

--- application-workloads/jboss/jboss-eap-standalone-rhel/scripts/jbosseap-setup-redhat.sh
+++ application-workloads/jboss/jboss-eap-standalone-rhel/scripts/jbosseap-setup-redhat.sh
@@ -63,13 +63,12 @@ then
     echo "Installing JAVA 8" | adddate >> jbosseap.install.log
     echo "sudo yum install java-1.8.0-openjdk -y" | adddate >> jbosseap.install.log
     sudo yum install java-1.8.0-openjdk -y | adddate >> jbosseap.install.log
-elif [ $JAVA_VERSION == "JAVA_11" ]
+  elif [ $JAVA_VERSION == "JAVA_11" ]
 then
     echo "Installing JAVA 11" | adddate >> jbosseap.install.log
     echo "sudo yum install java-11-openjdk -y" | adddate >> jbosseap.install.log
     sudo yum install java-11-openjdk -y | adddate >> jbosseap.install.log
-elif [ $JAVA_VERSION == "JAVA_17" ]
-then
+else
     echo "Installing JAVA 17" | adddate >> jbosseap.install.log
     echo "sudo yum install java-17-openjdk -y" | adddate >> jbosseap.install.log
     sudo yum install java-17-openjdk -y | adddate >> jbosseap.install.log
