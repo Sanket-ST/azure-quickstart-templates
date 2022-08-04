b2b57b764ce70a8e6616157626d324382efb7d1c
Sanket-ST <98595691+Sanket-ST@users.noreply.github.com>
2022-08-04 11:50:34 +0530

    Update jbosseap-setup-redhat.sh

--- application-workloads/jboss/jboss-eap-standalone-rhel/scripts/jbosseap-setup-redhat.sh
+++ application-workloads/jboss/jboss-eap-standalone-rhel/scripts/jbosseap-setup-redhat.sh
@@ -63,10 +63,15 @@ then
     echo "Installing JAVA 8" | adddate >> jbosseap.install.log
     echo "sudo yum install java-1.8.0-openjdk -y" | adddate >> jbosseap.install.log
     sudo yum install java-1.8.0-openjdk -y | adddate >> jbosseap.install.log
-else
+elif [ $JAVA_VERSION == "JAVA_11" ]
+then
     echo "Installing JAVA 11" | adddate >> jbosseap.install.log
     echo "sudo yum install java-11-openjdk -y" | adddate >> jbosseap.install.log
     sudo yum install java-11-openjdk -y | adddate >> jbosseap.install.log
+else
+    echo "Installing JAVA 17" | adddate >> jbosseap.install.log
+    echo "sudo yum install java-17-openjdk -y" | adddate >> jbosseap.install.log
+    sudo yum install java-17-openjdk -y | adddate >> jbosseap.install.log
 fi
 
 echo "subscription-manager attach --pool=EAP_POOL" | adddate >> jbosseap.install.log
